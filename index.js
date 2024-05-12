5c82fa941ffd8335bba0af586503f642728e4ee4
Grigorii Horos <horosgrisa@gmail.com>
2024-05-12 03:23:38 +0300

    Upgrade deps

--- index.js
+++ index.js
@@ -28,7 +28,100 @@ const md = markdownit({
   .use(markdownItSub)
   .use(markdownItTaskList)
   .use(alert, {
-    deep: true,
+    deep: false,
+
+    openRender(tokens, index, options, environment, self) {
+      const token = tokens[index];
+
+      let color = null;
+
+      // Case token.markup in ['important', 'note', 'tip', 'warning', 'caution']
+      switch (token?.markup) {
+        case 'important': {
+          color = 'red';
+
+          break;
+        }
+        case 'note': {
+          color = 'blue';
+
+          break;
+        }
+        case 'tip': {
+          color = 'green';
+
+          break;
+        }
+        case 'warning': {
+          color = 'yellow';
+
+          break;
+        }
+        case 'caution': {
+          color = 'yellow light';
+
+          break;
+        }
+        default: {
+          color = 'blue';
+        }
+      }
+
+      return `<blockquote class="x-cli-color-${color}">`;
+    },
+
+    closeRender(tokens, index, options, environment, self) {
+      return '</blockquote>';
+    },
+    titleRender: (tokens, index) => {
+      const token = tokens[index];
+
+      // console.log(token)
+
+      let color = null;
+      let icon = null;
+
+      switch (token?.markup) {
+        case 'important': {
+          color = 'red';
+          icon = '•';
+
+          break;
+        }
+        case 'note': {
+          color = 'blue';
+          icon = '•';
+
+          break;
+        }
+        case 'tip': {
+          color = 'green';
+          icon = '•';
+
+          break;
+        }
+        case 'warning': {
+          color = 'yellow';
+          icon = '•';
+
+          break;
+        }
+        case 'caution': {
+          color = 'yellow light';
+          icon = '•';
+
+          break;
+        }
+        default: {
+          color = 'blue';
+          icon = '•';
+        }
+      }
+
+      return `<p><span class="x-cli-color-${color}"> ${
+        token.content[0].toUpperCase() + token.content.slice(1).toLowerCase()
+      }</span></p>\n`;
+    },
   });
 
 md.renderer.rules.footnote_anchor = () => '';
